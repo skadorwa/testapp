c63515aa5ad1b59b407361c67ebf073900df364a
Paweł Skadorwa <pawel.skadorwa@outlook.com>
2019-03-13 09:19:09 +0100

    Update installmaven.sh

--- installmaven.sh
+++ installmaven.sh
@@ -1,1 +1,3 @@
-yum install -y maven
+yum install -y wget
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y apache-maven
